0abbe7e91de3ff236541f77db14a5637b5744f1f
Renao Chetri <63331185+ChhetriPrem@users.noreply.github.com>
2025-03-07 17:56:19 +0530

    Delete backend directory

--- backend/utils/jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-const jwtSecret = "renao";
-const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, jwtSecret, {
-    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
-  });
-};
-
-module.exports = generateToken;
