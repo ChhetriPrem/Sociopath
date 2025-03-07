4f25eeef9ee02ff3f2a1d4575ae2498937ff66e1
Renao Chetri <63331185+ChhetriPrem@users.noreply.github.com>
2025-03-07 19:36:58 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,28 +1,26 @@
 const express = require("express");
-
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const authMiddleware = require("../middleware/authMiddleware");
 const generateToken = require("../utils/jwt");
 
-// define the signup page
-
+// Signup route
 router.post("/signup", async (req, res) => {
   const { username, email, password, img, location, bio } = req.body;
 
   try {
-    // checking if user aleady exist
+    // Checking if the user already exists
     const existingUser = await User.findOne({ username });
     if (existingUser) {
-      return res.status(400).json({ message: "User already exist" });
+      return res.status(400).json({ message: "User already exists" });
     }
-    // encryping with salt and hashed password
 
+    // Encrypting password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // if not then resgister use
+    // Registering new user
     const newUser = new User({
       username,
       email,
@@ -32,62 +30,59 @@ router.post("/signup", async (req, res) => {
       location,
     });
     await newUser.save();
-    const token = generateToken(newUser._id);
 
+    const token = generateToken(newUser._id);
     res.cookie("token", token, {
       httpOnly: true,
       secure: false,
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-    res
-      .status(200)
-      .json({ message: "User successfully registered in the database" });
+
+    res.status(200).json({ message: "User successfully registered" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
 
-// let make the login system
-// todo 1. see if user exists , if yes match the email and password
+// Login route
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
-  // check the username exist
   try {
     const existingUser = await User.findOne({ email });
     if (!existingUser) {
       return res.status(400).json({
-        message:
-          "User is not registered in the database, Please register first!",
+        message: "User is not registered. Please register first!",
       });
     }
 
-    // if user exist check the password and email
-    const isPasswordValid = await bcrypt.compare(
-      password,
-      existingUser.password
-    );
-
+    const isPasswordValid = await bcrypt.compare(password, existingUser.password);
     if (!isPasswordValid) {
       return res.status(400).json({ message: "Wrong Credentials" });
     }
 
-    // save the token when logiin also
     const token = generateToken(existingUser._id);
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: false, // change to `true` in production
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.status(200).json({ message: "Login successful" });
-  } catch {}
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// Logout route
+router.post("/logout", (req, res) => {
+  res.clearCookie("token"); // Clears the token cookie
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 module.exports = router;
-
-// todo: to make logout functionality
